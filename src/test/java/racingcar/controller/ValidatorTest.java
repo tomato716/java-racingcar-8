210a69051b84e39c18dac65160d08324235c98aa
JunSeok_Park <pjh203203@naaver.com>
2025-10-26 22:45:51 +0900

    test: 쉼표로 이름 분리되는지 테스트 코드 추가

--- src/test/java/racingcar/controller/ValidatorTest.java
+++ src/test/java/racingcar/controller/ValidatorTest.java
@@ -1,5 +1,22 @@
 package racingcar.controller;
 
-public class ValidatorTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ValidatorTest {
+    private final Validator validator = new Validator();
+
+
+    @DisplayName("쉼표(,)를 기준으로 입력받은 문자열을 분리하여 리스트로 반환")
+    @Test
+    void splitCarName() {
+        String input = "ab,cd,ef";
+
+        List<String> carNames = validator.splitCarName(input);
+
+        assertEquals(List.of("ab", "cd", "ef"), carNames);
+    }
 }
