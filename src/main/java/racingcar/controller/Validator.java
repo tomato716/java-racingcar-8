f0489f570135bf6264622f6445ec71c035364de2
JunSeok_Park <pjh203203@naaver.com>
2025-10-26 22:42:37 +0900

    feat: 자동차 이름이 0이하 5이상이면 예외 발생 기능 추가

--- src/main/java/racingcar/controller/Validator.java
+++ src/main/java/racingcar/controller/Validator.java
@@ -1,7 +1,25 @@
 package racingcar.controller;
 
+import java.util.List;
+
 public class Validator {
     private static final String DELIMITER = ",";
+    private static final int NAME_LENGTH_LIMIT = 5;
+
+    public List<String> splitCarName(String carName) {
+        List<String> carNames = split(carName);
+        validateName(carNames);
+
+        return carNames;
+    }
+
+    private void validateName(List<String> carNames) {
+        for (String carName : carNames) {
+            if (carName.length() > NAME_LENGTH_LIMIT || carName.isBlank()) {
+                throw new IllegalArgumentException("자동차 이름은 1자 이상 5자 이하만 입력 가능합니다.");
+            }
+        }
+    }
 
     private List<String> split(String carName) {
 
