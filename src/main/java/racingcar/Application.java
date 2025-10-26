d75ddfa5bf9469234aa6772838a15e6819f4e511
JunSeok_Park <pjh203203@naaver.com>
2025-10-26 23:08:30 +0900

    feat: 전체 조립자 game 객체 의존성 주입 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,20 @@
 package racingcar;
 
+import racingcar.controller.Game;
+import racingcar.controller.Validator;
+import racingcar.model.Cars;
+import racingcar.view.Input;
+import racingcar.view.Output;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Input input = new Input();
+        Output output = new Output();
+        Validator validator = new Validator();
+        Cars cars = new Cars();
+        Game game = new Game(input, output, validator, cars);
+
+        game.playGame();
     }
 }
