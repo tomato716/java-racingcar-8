6876e7ee89d16a01006830f14393874fa9846676
JunSeok_Park <pjh203203@naaver.com>
2025-10-26 22:40:54 +0900

    feat: 쉼표로 문자열 분리 기능 추가

--- src/main/java/racingcar/controller/Validator.java
+++ src/main/java/racingcar/controller/Validator.java
@@ -1,5 +1,10 @@
 package racingcar.controller;
 
 public class Validator {
+    private static final String DELIMITER = ",";
 
+    private List<String> split(String carName) {
+
+        return List.of(carName.split(DELIMITER));
+    }
 }
