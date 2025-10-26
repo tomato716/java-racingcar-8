841ef942c355ef2fa985cea226c76a33b7874371
JunSeok_Park <pjh203203@naaver.com>
2025-10-26 22:56:49 +0900

    feat: 실행 결과 출력 기능 추가

--- src/main/java/racingcar/view/Output.java
+++ src/main/java/racingcar/view/Output.java
@@ -1,6 +1,10 @@
 package racingcar.view;
 
+import java.util.List;
+import racingcar.model.Car;
+
 public class Output {
+    private static final String EXPRESS = "-";
 
     public void carNameRequest() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -9,4 +13,16 @@ public class Output {
     public void gameAttemptRequest() {
         System.out.println("시도할 횟수는 몇 회인가요?");
     }
+
+    public void roundResult(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + printPosition(car.getPosition()));
+        }
+        System.out.println();
+    }
+
+    private String printPosition(int position) {
+        return EXPRESS.repeat(position);
+    }
+
 }
