977ec0eef59ab9b59fa2b1f4f004c3be69f64998
JunSeok_Park <pjh203203@naaver.com>
2025-10-27 11:35:03 +0900

    feat: 시도 횟수가 0이하라면 예외 발생 기능 추가

--- src/main/java/racingcar/controller/Validator.java
+++ src/main/java/racingcar/controller/Validator.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class Validator {
     private static final String DELIMITER = ",";
     private static final int NAME_LENGTH_LIMIT = 5;
+    private static final int ZERO = 0;
 
     public List<String> splitCarName(String carName) {
         List<String> carNames = split(carName);
@@ -26,11 +27,18 @@ public class Validator {
         return List.of(carName.split(DELIMITER));
     }
 
-    public void validateGameAttempt(String gameRound) {
+    public void validateGameAttempt(String input) {
         try {
-            Integer.parseInt(gameRound);
+            int gameRound = Integer.parseInt(input);
+            gameAttemptBiggerThanZero(gameRound);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("시도 횟수는 숫자만 입력할 수 있습니다.");
+        }
+    }
+
+    private void gameAttemptBiggerThanZero(int gameRound) {
+        if (gameRound <= ZERO) {
+            throw new IllegalArgumentException("시도 횟수는 0보다 커야합니다.");
         }
     }
 }
